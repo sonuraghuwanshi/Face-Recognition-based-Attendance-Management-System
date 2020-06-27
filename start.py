44a70f650aa924275f438d023e5924fc5e2aa84c
Sonu Raghuwanshi <sonu8823812809@gmail.com>
2020-06-28 01:59:05 +0530

    Add files via upload

--- start.py
+++ start.py
@@ -4,15 +4,25 @@ import cv2
 import numpy as np
 from PIL import Image
 import sqlite3
+import tkinter as tk
+
+
 faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml');
 cam=cv2.VideoCapture(0);
-
 def takeatt():
-    #ok()
     rec=cv2.face.LBPHFaceRecognizer_create();
     rec.read("recognizer\\trainningData.yml")
     id=0
     font=cv2.FONT_HERSHEY_SIMPLEX
+    checkset=set([0,0])
+    subject=var.get()
+    print(subject)
+    conn=sqlite3.connect("FaceBase.db")
+    cmd2="UPDATE People SET "+subject+"_Total="+subject+"_Total+1"
+    conn.execute(cmd2)
+    conn.commit()
+    conn.execute("UPDATE People SET Total=Total+1")
+    conn.commit()
     while(True):
         ret,img=cam.read();
         gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -22,6 +32,17 @@ def takeatt():
             id,conf=rec.predict(gray[y:y+h,x:x+w])
             profile=getProfile(id)
             if(profile!=None):
+                if id in checkset:
+                    print(profile[1]+" attendance marked")
+                else:
+                    checkset.add(id)
+                    #print(checkset)
+                    print("Attendance Marked")
+                    conn=sqlite3.connect("FaceBase.db")
+                    cmd2="UPDATE People SET Attendance= Attendance+1,"+subject+"="+subject+"+1 WHERE ID="+str(id)
+                    conn.execute(cmd2)
+                    conn.commit()
+                conn.close()
                 cv2.putText(img,str(profile[1]),(x,y+h),font,1,255,2);
                 cv2.putText(img,str(profile[2]),(x,y+h+30),font,1,255,2);
                 cv2.putText(img,str(profile[3]),(x,y+h+60),font,1,255,2);
@@ -45,6 +66,8 @@ def insertOrUpdate(Id,Name):
     else:
         cmd="INSERT INTO People(ID,Name) Values("+str(Id)+","+str(Name)+")"
     conn.execute(cmd)
+    conn.commit()
+    conn.execute("Update People SET Attendance=0,Operating_System=0,Data_Structure=0,Database_Management=0,Algorithm=0,Digital_Circuit=0,Percentage=0,Total=0 WHERE ID="+str(Id))
     conn.commit()
     conn.close()
 
@@ -106,18 +129,17 @@ def take_att():
     window=Tk()
     window.title("Take Attendance")
     window.geometry("600x400")
+    global var
     var=StringVar(window)
+    var.set('Select Subject')
     label2=Label(window,text="Attendance Successfully Completed",fg='blue',font=("arial",16,"bold")).place(x=190,y=30)
-    list1=['Operating System','Data Structure','Database Management','Algorithm','Digital Circuit']
-    var.set("Select Subject")
-    droplist=OptionMenu(window,var,*list1)
+    list1=['Operating_System','Data_Structure','Database_Management','Algorithm','Digital_Circuit']
+    droplist=OptionMenu(window, var, *list1)
     droplist.config(width=20)
     droplist.place(x=200,y=150)
     button_ta=Button(window,text="Take Attendance",fg='White',command=takeatt,bg='brown',relief=GROOVE,font=("arial",12,"bold"))
     button_ta.place(x=200,y=200)
     window.mainloop()
-    def ok():
-        print(var.get()) 
 
 
 def view_att():
@@ -125,16 +147,27 @@ def view_att():
     window.title("View Attendance")
     window.geometry("600x400")
     label2=Label(window,text="View Attendance",fg='blue',font=("arial",16,"bold")).place(x=190,y=30)
-
+    listbox=Listbox(window,height=15,width=80)
+    listbox.place(x=70,y=100)
+    conn=sqlite3.connect("FaceBase.db")
+    conn.execute("UPDATE People SET Percentage=Attendance*100/Total")
+    conn.commit()
+    result = conn.execute("SELECT * FROM People Order By ID")
+    for row in result.fetchall():
+        name = row[1]
+        per = row[16]
+        text=name+"    ----------      "+str(per)
+        listbox.insert(END,text)
+    
 
 def train_data():
     print("Machine Trained Successfully")
-    recognizer=cv2.face.LBPHFaceRecognizer_create();
+    recognizer=cv2.face.EigenFaceRecognizer_create()
     path='dataSet'
     Ids,faces=getImagesWithID(path)
     recognizer.train(faces,np.array(Ids))
     recognizer.save('recognizer/trainningData.yml')
-    cv2.destroyAllWindows()
+    cv2.destroyAllWindows(12,3,window)
     
 
 window=Tk()
